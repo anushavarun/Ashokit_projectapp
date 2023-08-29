c3f69e00025db4598f97163c4e5b89021f11fc6a
Anusha <vimalanamburi121@gmail.com>
2023-08-29 13:08:12 -0500

    java code is modified

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello first World!" );
+       
+         int a =10;
+int b=20;
+int c-30;
+int d=b+c;
+System.out.println(d);
     }
 }
